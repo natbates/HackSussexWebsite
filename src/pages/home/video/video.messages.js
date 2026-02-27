9355fd5ebd3c5249521c71403262bf404366a95a
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2026-02-27 17:26:39 +0000

    video

--- src/pages/home/video/video.messages.js
+++ src/pages/home/video/video.messages.js
@@ -1,7 +1,7 @@
 const videoMessages = {
-  heading: "See for yourself",
-  description: "Check out some videos all about us!",
-  ctaButton: "See more videos",
+    heading: "See for yourself",
+    description: "Check out some videos all about us!",
+    ctaButton: "See more videos",
 };
 
 export default videoMessages;
